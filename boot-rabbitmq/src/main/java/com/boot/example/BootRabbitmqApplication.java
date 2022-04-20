7a892537bfc2d56ab09cebbdc190d989e5f221c6
ywstz <1613710573@qq.com>
2022-04-20 16:10:59 +0800

    boot-example项目 增加boot-rabbitmq的初始化搭建 提交

---
+++ boot-rabbitmq/src/main/java/com/boot/example/BootRabbitmqApplication.java
@@ -0,0 +1,17 @@
+package com.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TuoZhou
+ * @date 2022/4/19
+ */
+@SpringBootApplication
+public class BootRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRabbitmqApplication.class, args);
+    }
+
+}
